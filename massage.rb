9209d0a9295ece048242195b02bfd63fc485127b
Monique Rio <nique.rio@gmail.com>
2016-06-24 16:43:25 -0400

    updated metadata to make sure archive and shelfmark are strings; added missing metadata files

--- massage.rb
+++ massage.rb
@@ -6,15 +6,23 @@ File.open('./temp.txt','r') do |file|
    file.each_line{ |dir|
        dir.chomp!
        t_file = Tempfile.new('filename_temp.txt')
-       File.open("./#{dir}/#{dir}.ly", 'r') do |f|
+       File.open("./#{dir}/metadata.yaml", 'r') do |f|
          f.each_line{|line| 
-          if line.match(/scDate/) 
-            line = "scDate = \"#{today}\""
-           end
+          if line.match(/archive/) 
+            split = line.split(':',2)
+            split[1].strip!
+            split[1].sub!(/\(/,'[')
+            split[1].sub!(/\)/,']')
+            line = split[0] + ": '#{split[1]}'"
+          elsif line.match(/shelfmark/) 
+            split = line.split(':',2)
+            split[1].strip!
+            line = split[0] + ": '#{split[1]}'"
+          end
             t_file.puts line
          }
        end
        t_file.close
-       FileUtils.mv(t_file.path, "./#{dir}/#{dir}.ly")
+       FileUtils.mv(t_file.path, "./#{dir}/metadata.yaml")
    }
 end
