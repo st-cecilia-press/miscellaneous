c65f29dbdc7a5e9ee2e005d5e397c2403212d557
Monique Rio <nique.rio@gmail.com>
2016-06-29 13:19:41 -0400

    updated README; updated ennatus metadata and list.rb

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# miscellaneous
+# Miscellaneous Pieces
+
+To create a new piece
+* git checkout -b piece_slug 
+* mkdir piece_slug
+* cd piece_slug
+* ruby ../include/vocal_template.ly.erb -f piece_slug.ly  -t? -p num_parts -v num_verses
+* ruby ../include/score.ly.erb -f piece_slug.ly
+* git add .
+* git commit -m "started piece_slug"
+* git push --set-upstream origin piece_slug
--- list.rb
+++ list.rb
@@ -1,35 +1,20 @@
 require 'fileutils'
 require 'tempfile'
+require 'yaml'
 
-manuscripts = Hash.new 
+books = Hash.new 
 directories = Dir.glob('*').select {|f| File.directory? f and f != "include" and f !=  "test"}
 directories.each do  |dir|
-  File.open("./#{dir}/metadata.yaml", 'r') do |f|
-    h = Hash.new
-    f.each_line { |line| 
-     if line =~ /archive/
-        line = line.split(':',2)
-        line = line[1].strip
-        h[:archive] = line
-     elsif line =~ /shelfmark/ 
-        line = line.split(':',2)
-        line = line[1].strip!
-        h[:shelfmark] = line
-     end  
-    }
-    if h[:archive]
-      slug = "archive: #{h[:archive]}; shelfmark: #{h[:shelfmark]}"
-      manuscripts[slug] = 'blah'
-    end
+  metadata = YAML.load_file("./#{dir}/metadata.yaml")
+  if metadata["books"] 
+      books[metadata["books"][0]["title"]] = metadata["books"][0]["date"]
   end
 end
 
-for key in  manuscripts.keys
-  key = key.split(';',2)
-  archive = key[0].split(':',2)
-  archive = archive[1].strip
-  shelfmark = key[1].split(':',2)
-  shelfmark = shelfmark[1].strip
+for key in books.keys
+  
+  title = key
+  date = books[key]
 
-  puts "{ name: , archive: #{archive}, shelfmark: #{shelfmark}, diamm: },"
+  puts "{ title: \"#{title}\", date: #{date}},"
 end
