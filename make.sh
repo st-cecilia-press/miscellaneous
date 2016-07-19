f8904648bd046c9447ce5f5ea4e3d2d76c43b083
Monique Rio <nique.rio@gmail.com>
2016-07-19 18:29:34 -0400

    generated midis and mp3s

--- make.sh
+++ make.sh
@@ -6,8 +6,16 @@ LILYPOND="lilypond --include $PWD/include"
 
 
 for folder in $(ls -d */ | egrep -v '(include|test)'); do 
-    ly_file="./${folder}${folder%%/}.ly";
-    $RUBY ./include/score.ly.erb $ly_file -o ./${folder}score.ly
-    $LILYPOND --include $PWD/${folder} --output=./${folder} $ly_file; 
+     if [ -e "./${folder}${folder%%/}.mid" ]
+     then
+        $RUBY ./include/midi_mp3.rb --mp3 "./${folder}${folder%%/}.mid";
+     fi
+     if [ -e "./${folder}${folder%%/}-transposed.mid" ]
+     then
+        $RUBY ./include/midi_mp3.rb --mp3 "./${folder}${folder%%/}-transposed.mid";
+     fi
+#    $RUBY ./include/score.ly.erb $ly_file -o ./${folder}score.ly
+#    $LILYPOND --include $PWD/${folder} --output=./${folder} $ly_file; 
+
 done
 
